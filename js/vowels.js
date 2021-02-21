30df5778e770474cd5bdce500a322f48ff5b8fdd
shikalchik <shikalchik@mail.ru>
2021-02-21 23:48:11 +0300

    vowels

---
+++ js/vowels.js
@@ -0,0 +1,39 @@
+'use strict'
+// debugger
+const getStr = prompt('Введите любую строчку:');
+
+function countingVowels(str) {
+    const vowels = {
+        letter1: 'а',
+        letter2: 'у',
+        letter3: 'о',
+        letter4: 'ы',
+        letter5: 'и',
+        letter6: 'э',
+        letter7: 'я',
+        letter8: 'ю',
+        letter9: 'е',
+        letter10: 'ё',
+        letter11: 'А',
+        letter12: 'У',
+        letter13: 'О',
+        letter14: 'Ы',
+        letter15: 'И',
+        letter16: 'Э',
+        letter17: 'Я',
+        letter18: 'Ю',
+        letter19: 'Е',
+        letter20: 'Ё',
+    }
+    var quatity = 0;  
+    for (var i = 0; i <= str.length-1; i++) {
+        for (var key in vowels) {
+            if (str[i] === vowels[key]) {
+                quatity++;
+            }
+        }
+    }
+    return console.log(quatity);
+};
+
+countingVowels(getStr);
