597fd027dd1eb1bb6478cdbed30bab25c505dbd5
shikalchik <shikalchik@mail.ru>
2021-02-27 23:37:11 +0300

    Drinks for your check

--- js/drinks_hash_func.js
+++ js/drinks_hash_func.js
@@ -1,28 +1,35 @@
 'use strict'
 
 function HashStorageFunc(name, age, sex, growth) {
-    this.name = name;
-    this.age = age;
-    this.sex = sex;
-    this.growth = growth;
-    this.addValue = function(key, value) {
-        return this[key] = value;
+
+    const that = this;
+
+    that.name = name;
+    that.age = age;
+    that.sex = sex;
+    that.growth = growth;
+
+    that.addValue = function(key, value) {
+        return that.key = value;
     };
-    this.getValue = function(key) {
-        return this[key];
+
+    that.getValue = function(key) {
+        return that.key;
     };
-    this.deleteValue = function(key) {
-        if (this[key] = null) {
+
+    that.deleteValue = function(key) {
+        that.key = null
+        if (that.key === null) {
             return true;
         } else {
             return false;
         };
     };
-    this.getKeys = function() {
-        return this.key;
+    
+    that.getKeys = function(key) {
+        return that.key;
     };
+    
 };
 
 let alex = new HashStorageFunc('Alex', 22, 'Man', 174);
-alex.addValue(big, yes)
-console.log(alex.big);
